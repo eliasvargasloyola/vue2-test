93c1def3a0694fa93bad01e7c2d48fb29a737edb
Elias Vargas <elias.vargas@globant.com>
2019-12-18 23:01:31 -0300

    Use components, Vuex and add Route

---
+++ my-profile/src/components/Lista.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="container">
+        <ul>
+            <li v-for="(item, index) of toSort" :key="item.nombre"
+                class="list-group-item d-flex justify-content-between align-items-center" @click="aumentar(index)">
+                {{index+1}} - {{item.nombre}}
+                <span class="badge badge-primary badge-pill">{{item.cantidad}}</span>
+            </li>
+            <button class="btn btn-danger btn-block" @click="reset">Reset</button>
+        </ul>
+    </div>
+</template>
+
+<script>
+    import {mapMutations, mapState} from "vuex";
+
+    export default {
+        name: "Lista",
+        computed: {
+            ...mapState(['frutas']),
+            toSort(){
+                const myArray = this.frutas;
+                return myArray.sort((a, b) => b.cantidad - a.cantidad);
+            }
+        },
+        methods: {
+            ...mapMutations(['aumentar', 'reset'])
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
--- my-profile/src/store/index.js
+++ my-profile/src/store/index.js
@@ -5,8 +5,19 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    frutas:[
+      {nombre: 'Platano', cantidad: 0},
+      {nombre: 'Pera', cantidad: 0},
+      {nombre: 'Kiwi', cantidad: 0}
+    ]
   },
   mutations: {
+    aumentar(state, index){
+      state.frutas[index].cantidad++;
+    },
+    reset(state){
+      state.frutas.map(frt => frt.cantidad = 0);
+    }
   },
   actions: {
   },
